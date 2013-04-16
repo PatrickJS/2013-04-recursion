d9b2503146715c59bdfc254a7201a2a063fe1b7d
Patrick <patrick@gdi2290.local>
2013-04-15 23:00:19 -0700

    Finished .stringifyJSON()

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,37 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+// // this is what you would do if you liked things to be easy:
+// // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to have to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
+// // but you don't so you're going to have to write it from scratch:
+var stringifyJSON = function(obj) {
+  console.log(obj);
+  // your code goes here. mmk
+  var result = '',
+      type = $.type(obj);
+
+  switch(type) {
+    case 'string':
+      result = '"' + obj + '"';
+      break;
+    case 'array':
+      result += '[';
+      for (var i = 0; i < obj.length; i++) {
+        result += '' + stringifyJSON(obj[i]) + ',';
+      }
+      result += ']';
+      break;
+    case 'object':
+      result += '{';
+        for(var key in obj){
+          if (key != 'functions' && key != 'undefined') {
+            result += '' + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+          }
+        }
+      result += '}';
+      break;
+    default:
+      result += '' + obj + '';
+    break;
+  }
+result = result.replace(/,}/, '}').replace(/,]/, ']');
+return result;
 };
